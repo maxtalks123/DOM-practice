bd3c6f42d707260b0ba58e4fdfc5c694a2d14943
Max Emerson <maxemerson2013@gmail.com>
2024-01-11 22:58:26 +0000

    all done

--- app.js
+++ app.js
@@ -5,12 +5,11 @@ const h1 = document.createElement("h1");
 h1.textContent = "This is a title";
 body.appendChild(h1);
 console.log(h1);
-function list(a, b, c) {
-  const a = document.createElement("a");
-  const b = document.createElement("b");
-  const c = document.createElement("c");
-  a.textContent = "user input";
-  b.textContent = "list b";
-  c.textContent = "list c";
-  console.log(list);
-}
+let listitems = "banana, apple, orange";
+const list = document.createElement("list");
+list.textContent = listitems;
+body.appendChild(list);
+console.log(list);
+// How I would remove h1.
+// const h1 = document.querySelector("h1");
+//h1.remove();
